5f802d3efeedccdc5ec659c33ae45963bbfe148c
psmzakaria <psmzakaria@gmail.com>
2018-07-25 15:58:13 +0800

    add env for production

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require("express");
 const logger = require("morgan");
 var cors = require("cors");
 
-
 const index = require("./routes/index");
 const booksRouter = require("./routes/books.js");
 const authors = require("./routes/authors.js");
@@ -10,7 +9,16 @@ const authors = require("./routes/authors.js");
 const app = express();
 app.use(logger("dev"));
 app.use(express.json());
-app.use(cors());
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    cors({
+      origin: process.env.ALLOWED_ORIGIN
+    })
+  );
+} else {
+  app.use(cors());
+}
+
 
 app.use("/", index);
 booksRouter(app);
