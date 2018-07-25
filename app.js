b3a95700c100937bed7a11bf54dafb0de8581239
psmzakaria <psmzakaria@gmail.com>
2018-07-25 14:11:24 +0800

    added cors

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
+var cors = require("cors");
+var app = express();
 
 const index = require("./routes/index");
 const booksRouter = require("./routes/books.js");
@@ -8,9 +10,10 @@ const authors = require("./routes/authors.js");
 const app = express();
 app.use(logger("dev"));
 app.use(express.json());
+app.use(cors());
 
 app.use("/", index);
-booksRouter(app)
+booksRouter(app);
 app.use("/authors", authors);
 
 module.exports = app;
